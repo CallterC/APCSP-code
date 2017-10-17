a13c151ad61c08cdcdb7900c627577c7adc5b97a
CallterC <2023328767@qq.com>
2017-10-17 16:10:29 +0800

    Add files via upload

---
+++ 20171017.py
@@ -0,0 +1,23 @@
+vegatable = "cauliflower"
+quanlity = len(vegatable)
+print(quanlity)
+
+last = vegatable[quanlity-1]
+print(last)
+
+dessert = "chocolate"
+drink = dessert[3:7]
+print(drink)
+
+breakfast = "pineapple"
+tree= breakfast[0:4]
+cone=breakfast[:4]
+
+flavor="strawberry"
+snack=flavor[5:10]
+print(snack)
+food=flavor[5:]
+print(food)
+
+icecream=flavor[:]
+print(icecream)
